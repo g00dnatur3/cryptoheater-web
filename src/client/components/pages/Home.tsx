0ee029a363da3a1108e939c2ece52868e0051b35
user <g00dnatur3@gmail.com>
2021-06-04 16:11:09 -0700

    updates

--- src/client/components/pages/Home.tsx
+++ src/client/components/pages/Home.tsx
@@ -18,24 +18,7 @@ export const Home: FC<{}> = () => {
   const [address, setAddress] = useState('')
   const payoutAddressRef = useRef();
 
-  const getFavicon = () => {
-    var favicon = undefined;
-    var nodeList = document.getElementsByTagName("link");
-    for (var i = 0; i < nodeList.length; i++)
-    {
-        if((nodeList[i].getAttribute("rel") == "icon")||(nodeList[i].getAttribute("rel") == "shortcut icon"))
-        {
-            favicon = nodeList[i].getAttribute("href");
-        }
-    }
-    return favicon;        
-}
-
   useEffect(() => {
-
-    const favicon = getFavicon()
-    console.log('favicon:', favicon)
-
     if (!existingSettings) {
       setLoading(true)
       loadSettings().then(val => {
