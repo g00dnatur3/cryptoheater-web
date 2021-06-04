2227af2806621e206a0f435172a4366072b28a1f
user <g00dnatur3@gmail.com>
2021-06-04 16:10:12 -0700

    updates

--- src/client/components/pages/Home.tsx
+++ src/client/components/pages/Home.tsx
@@ -18,7 +18,24 @@ export const Home: FC<{}> = () => {
   const [address, setAddress] = useState('')
   const payoutAddressRef = useRef();
 
+  const getFavicon = () => {
+    var favicon = undefined;
+    var nodeList = document.getElementsByTagName("link");
+    for (var i = 0; i < nodeList.length; i++)
+    {
+        if((nodeList[i].getAttribute("rel") == "icon")||(nodeList[i].getAttribute("rel") == "shortcut icon"))
+        {
+            favicon = nodeList[i].getAttribute("href");
+        }
+    }
+    return favicon;        
+}
+
   useEffect(() => {
+
+    const favicon = getFavicon()
+    console.log('favicon:', favicon)
+
     if (!existingSettings) {
       setLoading(true)
       loadSettings().then(val => {
@@ -143,7 +160,14 @@ export const Home: FC<{}> = () => {
       alignItems: 'center',
       justifyContent: 'space-between',
     }}>
-      <div style={{ fontSize: 14, color: 'grey' }}>C R Y P T O H E A T E R</div>
+      <div style={{
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+      }}>
+        <img src="/favicon.ico" style={{paddingRight: 2, width: 20, height: 20, marginBottom: 5}} />
+        <div style={{ fontSize: 14, color: 'grey' }}>C R Y P T O H E A T E R</div>
+      </div>
       <div style={{ fontSize: 14, color: 'grey' }}>v1.0.0</div>
     </div>
 
